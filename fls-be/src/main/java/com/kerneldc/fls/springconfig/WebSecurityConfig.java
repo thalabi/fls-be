029d6306febc64c328318ddca0c91ded101fc127
Tarif Halabi <tarif.halabi@gmail.com>
2025-05-28 16:18:40 -0400

    Secure actuator end points.

--- fls-be/src/main/java/com/kerneldc/fls/springconfig/WebSecurityConfig.java
+++ fls-be/src/main/java/com/kerneldc/fls/springconfig/WebSecurityConfig.java
@@ -7,11 +7,17 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.DelegatingJwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.extern.slf4j.Slf4j;
@@ -23,10 +29,13 @@ public class WebSecurityConfig {
 	
 	@Value("${application.security.disableSecurity:false}")
 	private boolean disableSecurity;
+	@Value("${application.security.actuator.username}")
+	private String actuatorUsername;
+	@Value("${application.security.actuator.password}")
+	private String actuatorPassword;
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
-			/* CorsConfigurationSource corsConfigurationSource, */ KeycloakJwtRolesConverter keycloakJwtRolesConverter)
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, KeycloakJwtRolesConverter keycloakJwtRolesConverter)
 			throws Exception {
 
 		DelegatingJwtGrantedAuthoritiesConverter authoritiesConverter =
@@ -38,47 +47,48 @@ public class WebSecurityConfig {
 						keycloakJwtRolesConverter);
 
 		// Set up http security to use the JWT converter defined above
-//		httpSecurity.oauth2ResourceServer().jwt()
-//				.jwtAuthenticationConverter(jwt -> new JwtAuthenticationToken(jwt, authoritiesConverter.convert(jwt), keycloakJwtRolesConverter.getUsername(jwt)));
 		httpSecurity.oauth2ResourceServer((oauth2) -> oauth2
-				.jwt((jwt) -> jwt.jwtAuthenticationConverter(jwtConv -> new JwtAuthenticationToken(jwtConv,
+				.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConv -> new JwtAuthenticationToken(jwtConv,
 						authoritiesConverter.convert(jwtConv), keycloakJwtRolesConverter.getUsername(jwtConv)))));
 
-		//return httpSecurity.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll())
-				// .authorizeRequests(authorizeRequests ->
-				// authorizeRequests.anyRequest().authenticated())
-				// .authorizeRequests(authorizeRequests ->
-				// authorizeRequests.anyRequest().hasRole("kerneldc-realm-user-role"))
-				// .authorizeRequests(authorizeRequests ->
-				// authorizeRequests.anyRequest().hasRole("SCOPE_PROFILE"))
-
-//		httpSecurity.authorizeRequests()
-//		.mvcMatchers("/sandboxController/noBearerTokenPing", "/actuator/*").permitAll()
-//		.mvcMatchers("/protected/sandboxController/getUserInfo").hasRole("realm_sso-app-user-role")
-//		//.mvcMatchers("/noBearerTokenPing").hasRole("realm_sso2-app-admin-role")
-//		;
 		if (disableSecurity) {
 			LOGGER.warn("*** appliction security is currently disabled ***");
 			LOGGER.warn("*** to enable set application.security.disableSecurity to false ***");
-			//httpSecurity.authorizeHttpRequests ().anyRequest().permitAll();
-			httpSecurity.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.anyRequest().permitAll());
+			httpSecurity.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.anyRequest().permitAll());
 		} else {
-			httpSecurity.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-			.requestMatchers("/appInfoController/*", "/pingController/*", "/actuator/*").permitAll()
-			.anyRequest().authenticated());
+			httpSecurity.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+			.requestMatchers("/appInfoController/*", "/pingController/*").permitAll());
+			httpSecurity.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+			.requestMatchers("/actuator/*").hasRole("ACTUATOR")).httpBasic();
+			httpSecurity.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.anyRequest().authenticated());
 		}
 		
 		httpSecurity.exceptionHandling(
 						exception -> exception.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
 								.accessDeniedHandler(new BearerTokenAccessDeniedHandler()));
 
-		httpSecurity.csrf(csrf -> csrf.disable())
-				.cors(Customizer.withDefaults())
-				;
+		httpSecurity.csrf(csrf -> csrf.disable());
+		httpSecurity.cors(Customizer.withDefaults());
 				
 		httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		
 		return httpSecurity.build();
 	}
+	
+	// test
+	@Bean
+	public UserDetailsService userDetailsService() {
+	    UserDetails admin = User.withUsername(actuatorUsername)
+	        .password(passwordEncoder().encode(actuatorPassword))
+	        .roles("ACTUATOR")  // this adds ROLE_ADMIN
+	        .build();
+
+	    return new InMemoryUserDetailsManager(admin);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder(); // or NoOpPasswordEncoder for testing only
+	}	
 	
 }
