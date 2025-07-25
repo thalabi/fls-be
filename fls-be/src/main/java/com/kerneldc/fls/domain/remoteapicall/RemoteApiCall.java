17e939df0d5fad24fef681a503453807cc748d87
Tarif Halabi <tarif.halabi@gmail.com>
2025-07-25 09:49:20 -0400

    Change db parameters column to json.

--- fls-be/src/main/java/com/kerneldc/fls/domain/remoteapicall/RemoteApiCall.java
+++ fls-be/src/main/java/com/kerneldc/fls/domain/remoteapicall/RemoteApiCall.java
@@ -2,6 +2,9 @@ package com.kerneldc.fls.domain.remoteapicall;
 
 import java.time.OffsetDateTime;
 
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
 import com.kerneldc.fls.domain.AbstractPersistableEntity;
 import com.kerneldc.fls.domain.LogicalKeyHolder;
 import com.kerneldc.fls.service.http.HttpRequestTypeEnum;
@@ -23,6 +26,7 @@ public class RemoteApiCall extends AbstractPersistableEntity {
 	@Setter(AccessLevel.NONE)
 	private HttpRequestTypeEnum request;
 	
+	@JdbcTypeCode(SqlTypes.JSON)
     private String parameters;
     
     @Setter(AccessLevel.NONE)
